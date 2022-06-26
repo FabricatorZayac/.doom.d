c0918ba4f2bb1296504f81f85221ab618cfadaf0
Eugene Fedchuk <fed4chuck@gmail.com>
2022-06-26 14:39:43 +0300

    initial commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-style
+   '(face trailing tabs spaces lines-tail newline indentation::tab indentation space-mark tab-mark newline-mark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
